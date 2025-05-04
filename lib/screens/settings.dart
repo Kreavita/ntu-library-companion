55888f395fd56ee952433b11eb979ee8521ad220
Kreavita <37907534+Kreavita@users.noreply.github.com>
2025-05-04 15:26:29 +0800

    License: greatly reduce in-App copyright notice, add LICENSE to repo; Bump Version to 1.3

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -131,7 +131,7 @@ class _SettingsPageState extends State<SettingsPage> {
                 ),
 
                 AboutListTile(
-                  applicationVersion: "Version 1.2",
+                  applicationVersion: "Version 1.3",
                   applicationIcon: ClipRRect(
                     borderRadius: BorderRadius.circular(
                       16.0,
@@ -142,21 +142,8 @@ class _SettingsPageState extends State<SettingsPage> {
                       height: 96,
                     ),
                   ),
-                  applicationLegalese: """
-Copyright (C) 2025  The NTU Library Companion Authors
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.""",
+                  applicationLegalese:
+                      "Copyright (C) 2025  The NTU Library Companion Authors",
                 ),
               ],
             ),
